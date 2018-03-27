c790597edd3bd8ba25c84462347b653a3f950b9e
kjelldg <Kjelldg@gmail.com>
2018-03-28 00:19:59 +0200

    Added comments to tests class.

--- src/calculatorTests/CalculatorTests.java
+++ src/calculatorTests/CalculatorTests.java
@@ -18,61 +18,64 @@ class CalculatorTests {
 
 	@Test
 	public void ReturnCorrectNumber() {
+		// ACT
 		double expected = 2;
 		double actual = Double.parseDouble(calculator.calculateExpression("1+1"));
+		// ASSERT
 		assertEquals(expected, actual);
 	}
 
 	@Test
 	public void ReturnsCorrectMultipliedNumber() {
-
 		// ACT
 		double expected = 12d;
 		double actual = calculator.multiply(3, 4);
-
 		// ASSERT
 		assertEquals(expected, actual);
 	}
 
 	@Test
 	public void ReturnsCorrectDividedNumber() {
-
 		// ACT
 		double expected = 5d;
 		double actual = calculator.divide(20, 4);
-
 		// ASSERT
 		assertEquals(expected, actual);
 	}
 
 	@Test
 	public void ReturnsCorrectAddedNumber() {
+		// ACT
 		double expected = 12d;
 		double actual = calculator.add(8, 4);
-
+		// ASSERT
 		assertEquals(expected, actual);
 	}
 
 	@Test
 	public void ReturnsCorrectSubtractedNumber() {
+		// ACT
 		double expected = 12d;
 		double actual = calculator.subtract(14, 2);
-
+		// ASSERT
 		assertEquals(expected, actual);
 	}
 
 	@Test
 	public void ReturnsCorrectModulus() {
+		// ACT
 		double expected = 0;
 		double actual = calculator.modulus(3, 3);
-
+		// ASSERT
 		assertEquals(expected, actual);
 	}
 
 	@Test
 	public void ResultIsInBetweenAllowedValues() {
+		// ACT
 		boolean resultMax = calculator.resultIsWithinRangeOfDouble(String.valueOf(Double.MAX_VALUE));
 		boolean resultMin = calculator.resultIsWithinRangeOfDouble(String.valueOf(Double.MIN_VALUE));
+		// ASSERT
 		assertEquals(true, resultMax);
 		assertEquals(true, resultMin);
 	}
